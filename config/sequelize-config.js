dbdb604b0ad7d2d3c67d5234d9b85cc33f808932
Amanda <amanda.fralve@gmail.com>
2024-12-01 22:32:22 -0300

    fix: project fixing - partials added

--- config/sequelize-config.js
+++ config/sequelize-config.js
@@ -1,13 +1,13 @@
-import { Sequelize } from "sequelize";
+// Importando o Sequelize
+import Sequelize from "sequelize";
 
-const connection =  new Sequelize({
-    dialect: "mysql",
-    host: "localhost",
-    username: "root",
-    password: "",
-    // Comentar linha de database na primeira execução da aplicação
-    database: "",
-    timezone: "-03:00"
-});
-
-export default connection;
+// Criando os dados de conexão com o banco de dados
+const connection = new Sequelize({
+    dialect: 'mysql', 
+    host: 'localhost',
+    username: 'root',
+    password: '', 
+    database: 'galeria', // Comente essa linha na primeira execução da aplicação
+    timezone: '-03:00'
+})
+export default connection
--- index.js
+++ index.js
@@ -1,54 +1,59 @@
-// Importando Express, Multer, Connection e Models
-import express from "express";
-import multer from "multer";
-import connection from "./config/sequelize-config";
-import Galeria from "./models/Galeria";
+import express from 'express'
+// Importando o multer
+import multer from 'multer'
+const app = express()
 
-const app = express();
+// Importando Connection
+import connection from "./config/sequelize-config.js"
 
-// Realizando conexão com o banco de dados
+import Galeria from "./models/Galeria.js"
+
+// Realizando a conexão com o banco de dados
 connection.authenticate().then(() => {
-    console.log("Conexão com o banco realizada com sucesso!");
+    console.log("Conexão com o banco de dados feita com sucesso!")
 }).catch((error) => {
     console.log(error)
 });
 
-// Criando o banco de dados se ele não existir
-connection.query(`CREATE DATABASE IF NOT EXISTS galeria`).then(() => {
+//  Criando o banco de dados se ele não existir
+connection.query(`CREATE DATABASE IF NOT EXISTS galeria;`).then(()=> {
     console.log("O banco de dados está criado.");
-}).catch((error) =>{
-    console.log(error);
+}).catch((error) => {
+    console.log(error)
 });
 
-// Configurações Pasta public e View Engine
-app.use(express.static("public"));
-app.use("view engine", "ejs");
+//Configurações
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
+const upload = multer({dest: "public/uploads/"})
 
-// Rota Principal
-app.get("/", (req,res) => {
-    Galeria.findAll().then((imagens) => {
+// ROTA PRINCIPAL
+app.get("/", (req,res) =>{
+    Galeria.findAll().then(imagens => {
         res.render("index", {
-            imagens:imagens
+            imagens : imagens
         })
-    })
-})
+    }).catch((error) => {
+        console.log(error)
+    });
+});
 
-// Rota de uploads
-app.get("/uploads", upload.single("file"), (req,res) => {
-    const file = req.file.filename;
+// ROTA DE UPLOAD
+app.post("/upload", upload.single("file"),(req,res) => {
+    const file = req.file.filename
     Galeria.create({
-        file:file
-    })
-    res.redirect("/")
-})
+        file : file
+    });
+    res.redirect("/");
+});
 
-// Inicialização
-const port = "8080";
+
+const port = 8081
 app.listen(port, (error) => {
     if(error){
         console.log(`Ocorreu um erro! ${error}`);
-    }else {
+    } else {
         console.log(`Servidor iniciado com sucesso em: http://localhost:${port}.`);
     }
-})
+});
